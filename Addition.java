cc5a63d7989cd43996d6c4550c6b93d9296cf854
Deepak7987 <dp641568@gmail.com>
2023-06-28 18:44:43 +0530

    done

---
+++ Addition.java
@@ -0,0 +1,22 @@
+package com.rays.core;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		
+		int a = 10;
+		int b = 100;
+		int c;
+		
+		c = a + b;
+		System.out.println(c);
+		
+		if (a>b) {
+			
+			System.out.println("a is grater than b");
+			
+		}else {
+			System.out.println("b is grater");
+		}
+	}
+}
